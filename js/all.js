eadc7939bba127740dfe9fefcd3080dd132d6ff0
whale_fine <whfjgir325@gmail.com>
2020-12-01 17:12:37 +0800

    s2

--- js/all.js
+++ js/all.js
@@ -56,6 +56,7 @@ function imgRecord(HTML_content,img_size){//記錄按過哪張圖片
 
 function showList(){
     let str="";
+    if(!row_record.textcontent) str= '<p style="font-size:36px;">目前沒有紀錄哦!</p>';
     for(let i=data.length-1;i>=0;i--){
         str += `<div class="${data[i].size_class}"><a href="#"><img src="${data[i].content}" alt=""></a></div>`;   
     }
